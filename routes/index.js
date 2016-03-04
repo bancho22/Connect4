ab7756750f172c7d775c809d422cc18c1543d63c
Bancho Petrov <banchopetrov@gmail.com>
2016-03-04 19:11:54 +0100

    Part 1 halfway done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var siteinfo = require('../siteinfo/siteinfo');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: siteinfo.siteTitle, info : siteinfo.siteInfo, authors : siteinfo.authors });
 });
 
 module.exports = router;
---
+++ siteinfo/siteinfo.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Bancho on 04-Mar-16.
+ */
+
+var siteinfo = {
+    siteTitle : 'Connect 4',
+    siteInfo : 'The popular game now available for your browser (Android support soon)',
+    authors : ['zackargyle', 'lam', 'cph-bp73']
+};
+
+module.exports = siteinfo;
